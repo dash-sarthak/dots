884a01224fb9640eccba8fc6edc4640d67af7854
Sarthak Dash <dash.sarthak.work@gmail.com>
2024-07-27 08:49:26 +0530

    Projectile and other changes

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -4,9 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" default))
+   '("014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "c1d5759fcb18b20fd95357dcd63ff90780283b14023422765d531330a3d3cec2" "dfb1c8b5bfa040b042b4ef660d0aab48ef2e89ee719a1f24a4629a0c5ed769e8" "67f6b0de6f60890db4c799b50c0670545c4234f179f03e757db5d95e99bac332" "e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" default))
  '(package-selected-packages
-   '(counsel-projectile gcmh evil-nerd-commenter vterm-toggle vterm projectile hydra evil-collection evil general which-key rainbow-delimiters helpful doom-modeline all-the-icons counsel ivy-rich ivy)))
+   '(evil-magit magit counsel-projectile gcmh evil-nerd-commenter vterm-toggle vterm projectile hydra evil-collection evil general which-key rainbow-delimiters helpful doom-modeline all-the-icons counsel ivy-rich ivy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
