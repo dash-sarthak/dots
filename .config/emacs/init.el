ff8d89916cb0a9501eff83f50136ab5d6bd4c2ef
Sarthak Dash <dash.sarthak.work@gmail.com>
2024-08-07 13:22:11 +0530

    Finalized emacs config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -6,7 +6,7 @@
 (load custom-file)
 
 
-;; UI Cleanup
+;; UI Cleanup (setq inhibit-startup-message)
 (setq inhibit-startup-message t)    ; No splash screen
 (scroll-bar-mode -1)                ; Bye bye scrollbar
 (tool-bar-mode -1)                  ; Adios toolbar
@@ -27,9 +27,34 @@
 		    :font "JetBrainsMono Nerd Font"
 		    :weight 'regular
 		    :height 132)
+(set-face-attribute 'fixed-pitch nil
+		    :font "JetBrainsMono Nerd Font"
+		    :weight 'regular
+		    :height 132)
+
+(set-face-attribute 'variable-pitch nil
+		    :font "Inter"
+		    :weight 'regular
+		    :height 144)
+
 (set-face-attribute 'mode-line nil :weight 'medium :height 120)
 
 ;; Theme
+;; Configure the Modus Themes' appearance
+(setq modus-themes-mode-line '(borderless)
+      modus-themes-bold-constructs t
+      modus-themes-italic-constructs t
+      modus-themes-fringes 'subtle
+      modus-themes-paren-match '(bold intense)
+      modus-themes-prompts '(bold intense)
+      modus-themes-completions '(extrabold intense)
+      modus-themes-scale-headings t
+      ;; modus-themes-region '(bg-only)
+      modus-themes-headings
+      '((1 . (rainbow underline))
+        (2 . (rainbow))
+        (3 . (rainbow))))
+;; Load the dark theme
 (load-theme 'modus-vivendi t)
 
 ;; Line and column numbers
@@ -43,28 +68,25 @@
                 eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-;; PACKAGE MANAGER
-;; Initialize package sources
 
+;; Initialize package sources --------------------------------------------------
 (require 'package)
-
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/")
                          ("elpa" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
-
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Add packages
+
+;; Essential packages ----------------------------------------------------------
 ;; Non-intrusive garbage collection
 (use-package gcmh
   :demand
   :config
   (gcmh-mode 1))
-
 ;; Ivy - Swanky search and completion framework
 (use-package ivy
   :diminish
@@ -83,13 +105,11 @@
          ("C-d" . ivy-reverse-i-search-kill))
   :config
   (ivy-mode 1))
-
 ;; Ivy-rich - Make Ivy even richer
 (use-package ivy-rich
   :ensure t
   :init
   (ivy-rich-mode 1))
-
 ;; Counsel - More Ivy magic
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
@@ -99,14 +119,12 @@
          ("C-r" . 'counsel-minibuffer-history))
   :config
   (setq ivy-initial-inputs-alist nil))
-
 ;; WhichKey - Never forget your keybindings
 (use-package which-key
   :init (which-key-mode)
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 0))
-
 ;; Helpful - A better *help* buffer
 (use-package helpful
   :ensure t
@@ -118,11 +136,9 @@
   ([remap describe-command] . helpful-command)
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
-
 ;; Icons - Pretty icons for a prettier Emacs
 ;; Run M-x all-the-icons-install-fonts on new machines
 (use-package all-the-icons)
-
 ;; General - Easier keybindings
 (use-package general
   :config
@@ -131,7 +147,6 @@
     :keymaps '(normal insert visual emacs)
     :prefix "SPC"
     :global-prefix "C-SPC"))
-
 ;; Evil - Vim emulation
 (use-package evil
   :init
@@ -150,25 +165,14 @@
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal)
   (evil-set-undo-system 'undo-redo))
-
 ;; Evil everywhere
 (use-package evil-collection
   :after evil
   :config
   (evil-collection-init))
-
 ;; Teminal emulation
 (use-package vterm)
-
-;; Toggle terminal
 (use-package vterm-toggle)
-
-;; Comments
-(use-package evil-nerd-commenter
-  :general
-  (general-nvmap
-    "gc" 'evilnc-comment-operator))
-
 ;; Projectile
 (use-package projectile
   :diminish projectile-mode
@@ -179,28 +183,58 @@
   (when (file-directory-p "~/Development")
     (setq projectile-project-search-path '("~/Development")))
   (setq projectile-switch-project-action #'projectile-dired))
-
 ;; Counsel-projectile
 (use-package counsel-projectile
   :config (counsel-projectile-mode))
-
 ;; Doom modeline
 (use-package doom-modeline
   :init (doom-modeline-mode 1))
-
 ;; Magit
 (use-package magit)
 
 
-;; Keybindings
+;; Leader keybindings ----------------------------------------------------------
 (dash/leader-key
-  ;; Projects
   "p" 'projectile-command-map
- 
   "t"  '(:ignore t :which-key "toggles")
   "t <escape>" '(keyboard-escape-quit :which-key t)
   "tt" '(counsel-load-theme :which-key "choose theme")
-  "ts" '(hydra-text-scale/body :which-key "scale text")
-  "RET" '(vterm-toggle :which-key "terminal"))
+  "RET" '(vterm-toggle :which-key "terminal")
+  "bb" '(counsel-switch-buffer :which-key "active buffers")
+  )
+
+
+;; Org Mode Configuration ------------------------------------------------------
+(defun dash/org-mode-setup ()
+  (org-indent-mode)
+  (visual-line-mode 1))
+
+(use-package org
+  :hook (org-mode . dash/org-mode-setup)
+  :config
+  (setq org-ellipsis " … ")
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "●" "○" "●" "○" "●" "○")))
+
+(defun dash/org-mode-visual-fill ()
+  (setq visual-fill-column-width 80
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . dash/org-mode-visual-fill))
+
+
+;; LSP -------------------------------------------------------------------------
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  (lsp-enable-which-key-integration t))
 
 
